1fd4661b4180239edd1b9dd6c6829d679263381d
Felix Wang <wangfelix87@gmail.com>
2018-02-09 13:19:57 +0800

    update ps.sh

---
+++ ps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Setup environment
+source "$DIR/env.sh"
+
+# Bring down the notebook container, using container name as project name
+CONFIG="notebook.yml"
+
+case $NAME in
+    opencv)
+    	CONFIG=opencv-notebook.yml
+    ;;
+    base-ocr)
+		CONFIG=base-ocr-notebook.yml
+    *) # unknown option
+    ;;
+esac
+docker-compose -f "$DIR/$CONFIG" -p "$NAME" ps
+~
